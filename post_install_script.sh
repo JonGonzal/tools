fa481051286f6a5c66930c419f2df090a4875d23
jongonzal <12769872+JonGonzal@users.noreply.github.com>
2023-02-27 15:08:47 -0600

    added post instal script

---
+++ post_install_script.sh
@@ -0,0 +1,38 @@
+
+
+echo "updating"
+sudo apt update; sudo apt upgrade -y
+
+
+sudo apt install i3
+
+sudo apt install tmux
+
+sudo apt install git
+
+sudo apt install gimp
+
+
+# Installing latest chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+rm google-chrome-stable_current_amd64.deb
+
+
+
+## Instaing latest nvim and allow for global access of the command "nvim"
+# Also adds nvim dependencies 
+wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage nvim
+mv ~/nvim /user/bin/
+
+sudo apt install python3-neovim
+
+sudo apt install npm
+
+sudo apt install clang
+
+
