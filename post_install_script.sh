9abb630e652f1044aa8e5bd930472341580452b1
jon <jon@plexserver.lan>
2023-02-28 00:58:07 +0000

    specify download location

--- post_install_script.sh
+++ post_install_script.sh
@@ -1,27 +1,27 @@
 
 
-echo "updating"
-sudo apt update; sudo apt upgrade -y
+#sudo apt update; sudo apt upgrade -y
 
 
 
-sudo apt install tmux
+sudo apt install tmux -y
 
-sudo apt install git
+sudo apt install git -y
 
+sudo apt install termius -y
 
-# Check if display exist
+# Check's if display exists and install GUI apps 
 if [ -n "${DISPLAY+x}" ]; then 
-	sudo apt install i3
+	sudo apt install i3 -y
 
-	sudo apt install gimp
+	sudo apt install gimp -y
 
 
 
 	# Installing latest chrome
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-	sudo apt install ./google-chrome-stable_current_amd64.deb
+	sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
 	rm google-chrome-stable_current_amd64.deb
 
@@ -29,15 +29,34 @@ fi
 
 ## Instaing latest nvim and allow for global access of the command "nvim"
 # Also adds nvim dependencies 
-wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-mv nvim.appimage nvim
-mv ~/nvim /user/bin/
+user_dir = /home/$USER
+wget -P $user_dir https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 
-sudo apt install python3-neovim
+ls $user_dir
 
-sudo apt install npm
+sudo chmod u+x $user_dir/nvim.appimage
 
-sudo apt install clang
+echo " next steps"
+
+#mv "~/nvim.appimage" "~/nvim"
+ls -lah ~/
+
+#ls -lah /usr/bin | grep "nv"
+#sudo ln -s ~/nvim /usr/bin/
 
 
+
+#sudo apt install python3-neovim -y
+
+#sudo apt install npm -y
+
+#sudo apt install clang -y
+
+## Kickstarting nvim 
+
+#git clone https://github.com/JonGonzal/kickstart.nvim.git
+
+#mv "kickstart.nvim/" "nvim"
+
+
+#sudo mv "nvim/" "~/.config/"
