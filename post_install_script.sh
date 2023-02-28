01eea015feebec216f0a04430c2bcd4a509fdc2b
jon <jon@plexserver.lan>
2023-02-28 03:57:48 +0000

    added the dotfiles to post_install_script

--- post_install_script.sh
+++ post_install_script.sh
@@ -1,8 +1,12 @@
+## Personal linux env for dev
 
+sudo apt update; sudo apt upgrade -y
 
-#sudo apt update; sudo apt upgrade -y
+sudo apt install python3-neovim -y
 
+sudo apt install npm -y
 
+sudo apt install clang -y
 
 sudo apt install tmux -y
 
@@ -36,31 +40,33 @@ if [ -n "${DISPLAY+x}" ]; then
 
 fi
 
-## Instaing latest nvim and allow for global access of the command "nvim"
+# Instaing latest nvim and allow for global access of the command "nvim"
 # Also adds nvim dependencies 
 
-#wget -P ~/ https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-#
-#sudo chmod u+x ~/nvim.appimage
-#
-#mv ~/nvim.appimage ~/nvim
-#
-#sudo ln -s ~/nvim /usr/bin/
-#
-#ls -lah /usr/bin | grep "nv"
+wget -P ~/ https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+
+sudo chmod u+x ~/nvim.appimage
+
+mv ~/nvim.appimage ~/nvim
+
+sudo ln -s ~/nvim /usr/bin/
 
 
-#sudo apt install python3-neovim -y
 
-#sudo apt install npm -y
-
-#sudo apt install clang -y
-
-## Kickstarting nvim 
-
+# Kickstarting nvim 
 # Creates the NVIM environment
 git clone https://github.com/JonGonzal/kickstart.nvim.git ~/Documents/repos/
 
 sudo mv ~/Documents/repos/kickstart.nvim/ ~/.config/
 
 mv ~/.config/kickstart.nvim/ ~/.config/nvim
+
+
+
+# Installs personal configs 
+git clone https://github.com/JonGonzal/tools.git ~/Documents/repos/
+
+sudo cp ~/Documents/repos/tools/.bashrc ~/
+sudo cp ~/Documents/repos/tools/.tmux.config ~/
+
+
